c35f7accf50bee5b36bf6b9af4c170581e182095
Arniel <aceba1@outlook.com>
2020-03-04 08:34:57 -0500

    Simple player movement

---
+++ Assets/PlayerTopDownMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTopDownMove : MonoBehaviour
+{
+    [Tooltip("Units per second")]
+    public float MoveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"),
+            0f) * MoveSpeed * Time.deltaTime;
+    }
+}
