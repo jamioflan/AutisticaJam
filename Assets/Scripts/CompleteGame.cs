c0a262f60ae2d00b73bb15cd69169a2ae371e6cb
DjayDjay94 <jjfoxanimator@gmail.com>
2020-03-09 03:57:11 +0000

    Game is complete

---
+++ Assets/Scripts/CompleteGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompleteGame : MonoBehaviour
+{ 
+    public GameObject whiteFade;
+
+    private void Start()
+    {
+        whiteFade.SetActive (false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Devin")
+        {
+            whiteFade.SetActive (true);
+
+            SceneManager.LoadScene("ThanksForPlaying");
+        }
+    }
+}
+
